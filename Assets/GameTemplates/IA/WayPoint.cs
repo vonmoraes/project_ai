1b28cf4435cd625820136f782ba2f4595126ce76
vonmoraes <moraessouzalucas@gmail.com>
2020-10-04 00:46:13 -0300

    Graph Edges working
    Diagnonal ainda falta criar a lógica para diagonais deletadas.

--- Assets/GameTemplates/IA/WayPoint.cs
+++ Assets/GameTemplates/IA/WayPoint.cs
@@ -1,23 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/**
+ * Waypoint :: Utilizado para atualização dos vértices, ou seja, ao mover ou deletar um vértice.
+ */
 [ExecuteInEditMode]
 public class WayPoint : MonoBehaviour
 {
+    
     private void OnEnable()
     {
-        // DO Stuff Here
+        int index = 0;
+        if (int.TryParse(name, out index) == false)
+        {
+            return;
+        }
+        transform.parent.parent.GetComponent<WayPointGrid>().UpdateVertexAvailability(index, true);
     }
 
     private void OnDisable()
     {
-        // DO Stuff Here
+        int index = 0;
+        if (int.TryParse(name, out index) == false)
+        {
+            return;
+        }
+        transform.parent.parent.GetComponent<WayPointGrid>().UpdateVertexAvailability(index, false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // DO Stuff Here
+        int index = 0;
+        if (int.TryParse(name, out index) == false)
+        {
+            return;
+        }
+        transform.parent.parent.GetComponent<WayPointGrid>().UpdateVertexPosition(index, transform.position);
     }
 }
