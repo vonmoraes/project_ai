87214cc19ece2947791f5dda0e3c6d8caf868257
Lucas de Souza Moraes <lucas.moraes.989785@sga.pucminas.br>
2020-10-06 23:01:48 -0300

    Update WayPoint.cs

--- Assets/GameTemplates/IA/WayPoint.cs
+++ Assets/GameTemplates/IA/WayPoint.cs
@@ -10,31 +10,16 @@ public class WayPoint : MonoBehaviour
     
     private void OnEnable()
     {
-        int index = 0;
-        if (int.TryParse(name, out index) == false)
-        {
-            return;
-        }
-        transform.parent.parent.GetComponent<WayPointGrid>().UpdateVertexAvailability(index, true);
+
     }
 
     private void OnDisable()
     {
-        int index = 0;
-        if (int.TryParse(name, out index) == false)
-        {
-            return;
-        }
-        transform.parent.parent.GetComponent<WayPointGrid>().UpdateVertexAvailability(index, false);
+
     }
 
     void Update()
     {
-        int index = 0;
-        if (int.TryParse(name, out index) == false)
-        {
-            return;
-        }
-        transform.parent.parent.GetComponent<WayPointGrid>().UpdateVertexPosition(index, transform.position);
+
     }
 }
